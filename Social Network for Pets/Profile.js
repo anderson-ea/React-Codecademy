0e3b9ea81fda37787789b20e74b7c732039dbd15
Eric Anderson <anderson.ericandrew@gmail.com>
2021-11-18 17:50:11 -0700

    Add social network for pets project

---
+++ Social Network for Pets/Profile.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { fetchUserData, cancelFetch } from './dataFetcher';
+import { Userlist } from './Userlist';
+
+export class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { userData: null };
+  }
+  loadUserData() {
+    this.setState({ userData: null });
+    this.fetch = fetchUserData(this.props.username, 
+    (userData) => {
+      this.setState({ userData });
+    });  
+  }
+  componentDidMount() {
+    this.loadUserData();
+  }
+  componentWillUnmount() {
+    cancelFetch(this.fetchID);
+  }
+  componentDidUpdate(prevProps) {
+    if (this.props.username !== prevProps.username) {
+      cancelFetch(this.fetchID);
+      this.loadUserData();
+    }
+  }
+  render() {
+    const isLoading = this.state.userData === null;
+    const name = isLoading ? 'Loading...' : this.state.userData.name;
+    const bio = isLoading ? 'Bio' : this.state.userData.bio;
+    const friends = isLoading ? [] : this.state.userData.friends;
+    const pic = isLoading ? null : this.state.userData.profilePictureUrl;
+    let className = 'Profile';
+    if (isLoading) {
+      className += ' loading';
+    }
+
+    return (
+      <div className={className}>
+        <div className="profile-picture">
+          <img src={pic} alt="prof-pic" />
+        </div>
+        <div className="profile-body">
+          <h2>{name}</h2>
+          <h3>@{this.props.username}</h3>
+          <p>{bio}</p>
+          <h3 onClick="">{friends}</h3>
+          <Userlist usernames={friends} onChoose={this.props.onChoose} />
+        </div>
+      </div>
+    );
+  }
+}
